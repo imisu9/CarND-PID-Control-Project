f35b35124fba2ad1e3435b9a5377c310612aa41b
Seo, Jungwoo <36559904+imisu9@users.noreply.github.com>
2019-05-06 18:22:41 +0900

    feat: implement twiddle

--- src/PID.cpp
+++ src/PID.cpp
@@ -35,7 +35,79 @@ void PID::UpdateError(double cte) {
 
 double PID::TotalError() {
   /**
+   * Initialize twiddle variables
+   */
+  double dKp = 1.0;
+  double dKi = 1.0;
+  double dKd = 1.0;
+  double tolerance = 0.2; // initialized as shown in the class
+  double best_error;
+  double curr_error;
+  //int iteration = 1;
+  
+  /**
    * TODO: Calculate and return the total error
    */
-  return (Kp*p_error + Ki*i_error + Kd*d_error);
+  best_error = Kp*p_error + Ki*i_error + Kd*d_error;
+  while ((dKp+dKi+dKd) > tolerance) {
+    //std::cout << "Iteration: " << iteration
+    //          < " best_error: " << best_error << std::endl;
+    // Proportional
+    Kp += dKp;
+    curr_error = Kp*p_error + Ki*i_error + Kd*d_error;
+    if (curr_error < best_error) {
+      best_error = curr_error;
+      dKp *= 1.1;
+    } else {
+      // Move to the opposite direction
+      Kp -= 2*dKp;
+      curr_error = Kp*p_error + Ki*i_error + Kd*d_error;
+      if (curr_error < best_error) {
+        best_error = curr_error;
+        dKp *= 1.1;
+      } else {
+        Kp += dKp;
+        dKp *= 0.9;
+      }
+    }
+    
+    // Integral
+    Ki += dKi;
+    curr_error = Kp*p_error + Ki*i_error + Kd*d_error;
+    if (curr_error < best_error) {
+      best_error = curr_error;
+      dKi *= 1.1;
+    } else {
+      // Move to the opposite direction
+      Ki -= 2*dKi;
+      curr_error = Kp*p_error + Ki*i_error + Kd*d_error;
+      if (curr_error < best_error) {
+        best_error = curr_error;
+        dKi *= 1.1;
+      } else {
+        Ki += dKi;
+        dKi *= 0.9;
+      }
+    }
+    
+    // Differential
+    Kd += dKd;
+    curr_error = Kp*p_error + Ki*i_error + Kd*d_error;
+    if (curr_error < best_error) {
+      best_error = curr_error;
+      dKd *= 1.1;
+    } else {
+      // Move to the opposite direction
+      Kd -= 2*dKd;
+      curr_error = Kp*p_error + Ki*i_error + Kd*d_error;
+      if (curr_error < best_error) {
+        best_error = curr_error;
+        dKd *= 1.1;
+      } else {
+        Kd += dKd;
+        dKd *= 0.9;
+      }
+    }
+  }
+  return best_error;
 }
