2ef5e214e124a5edaaa4db48efecb94792a91d4f
Seo, Jungwoo <36559904+imisu9@users.noreply.github.com>
2019-05-06 11:44:18 +0900

    feat: test coefficients of 0.5s

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
-  pid.Init(0.0, 0.0, 0.0);
+  pid.Init(0.5, 0.5, 0.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
