e030ba7bbf8bb37826e0ee791e60315b8934a04c
Seo, Jungwoo <36559904+imisu9@users.noreply.github.com>
2019-05-11 18:37:25 +0900

    feat: disable error variable initialization
    Init() will be used in twiddle to update private variables: Kp, Ki, Kd.

--- src/PID.cpp
+++ src/PID.cpp
@@ -18,7 +18,7 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   Kd = Kd_;
   
   // Need to figure out how to implement "if needed"
-  if (true) {
+  if (false) {
     p_error = 0.0;
     i_error = 0.0;
     d_error = 0.0;
