e547437de2c3200b9501e89026b263c91a4f0dea
Seo, Jungwoo <36559904+imisu9@users.noreply.github.com>
2019-05-11 23:32:48 +0900

    feat: finalize the order

--- src/PID.cpp
+++ src/PID.cpp
@@ -34,8 +34,8 @@ void PID::UpdateError(double cte) {
    * TODO: Update PID errors based on cte.
    */
   d_error = cte - p_error;
-  p_error = cte;
   i_error += cte;
+  p_error = cte;
 }
 
 double PID::TotalError() {
