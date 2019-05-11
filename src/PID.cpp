0d1d4c83c4969c82afd6640062014e27e1fbadc3
Seo, Jungwoo <36559904+imisu9@users.noreply.github.com>
2019-05-11 22:12:33 +0900

    feat: reorder assignments

--- src/PID.cpp
+++ src/PID.cpp
@@ -29,9 +29,9 @@ void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-  d_error = cte - p_error;
   p_error = cte;
   i_error += cte;
+  d_error = cte - p_error;
 }
 
 double PID::TotalError() {
